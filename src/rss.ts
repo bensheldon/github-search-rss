b9cc4035ab28295231654686e92115449825ec91
Ben Sheldon <bensheldon@gmail.com>
2021-10-11 19:51:16 -0700

    Update RSS Feeds

--- src/rss.ts
+++ src/rss.ts
@@ -4,49 +4,56 @@
  **/
 import { RSSItem } from "./index";
 
-const BASE_URL = "https://azu.github.io/github-search-rss";
+const BASE_URL = "https://bensheldon.github.io/github-search-rss";
 // Query references
 // https://docs.github.com/en/github/searching-for-information-on-github/understanding-the-search-syntax
 // https://docs.github.com/en/github/searching-for-information-on-github/about-searching-on-github
 // TYPE references
 // https://docs.github.com/en/graphql/reference/queries#searchresultitemconnection
 export const SEARCH_ITEMS: RSSItem[] = [
-    // Issue
     {
-        title: "microsoft/TypeScript Iteration Plan",
-        query: "repo:microsoft/TypeScript is:issue label:Planning",
+        title: "rails/rails ActiveJob Topics",
+        query: "repo:rails/rails is:issue ActiveJob Job",
         TYPE: "ISSUE",
-        link: `${BASE_URL}/typescript-iterator-plan.json`
-    },
-    {
-        title: "w3ctag/design-reviews Design Issues",
-        query: "repo:w3ctag/design-reviews is:issue",
-        TYPE: "ISSUE",
-        link: `${BASE_URL}/w3ctag-design-reviews.json`
-    },
-    // Pull Request
-    {
-        // label:data:
-        title: "mdn/browser-compat-data update data",
-        query: "repo:mdn/browser-compat-data is:pr is:open",
-        TYPE: "ISSUE",
-        link: `${BASE_URL}/mdn-browser-compat-data.json`,
-        filter: (item) => {
-            return item.labels.some((label) => label.startsWith("data:"));
-        }
-    },
-    {
-        // label:data:
-        title: "mdn/content update content",
-        query: "repo:mdn/content is:pr is:open",
-        TYPE: "ISSUE",
-        link: `${BASE_URL}/mdn-content.json`
-    },
-    // Repository
-    {
-        title: "LightWeight JavaScript repositories",
-        query: "lightweight language:javascript language:typescript sort:updated-desc",
-        TYPE: "REPOSITORY",
-        link: `${BASE_URL}/lightweight-javascript-repo.json`
+        link: `${BASE_URL}/rails-activejob.json`
     }
+
+    // // Issue
+    // {
+    //     title: "microsoft/TypeScript Iteration Plan",
+    //     query: "repo:microsoft/TypeScript is:issue label:Planning",
+    //     TYPE: "ISSUE",
+    //     link: `${BASE_URL}/typescript-iterator-plan.json`
+    // },
+    // {
+    //     title: "w3ctag/design-reviews Design Issues",
+    //     query: "repo:w3ctag/design-reviews is:issue",
+    //     TYPE: "ISSUE",
+    //     link: `${BASE_URL}/w3ctag-design-reviews.json`
+    // },
+    // // Pull Request
+    // {
+    //     // label:data:
+    //     title: "mdn/browser-compat-data update data",
+    //     query: "repo:mdn/browser-compat-data is:pr is:open",
+    //     TYPE: "ISSUE",
+    //     link: `${BASE_URL}/mdn-browser-compat-data.json`,
+    //     filter: (item) => {
+    //         return item.labels.some((label) => label.startsWith("data:"));
+    //     }
+    // },
+    // {
+    //     // label:data:
+    //     title: "mdn/content update content",
+    //     query: "repo:mdn/content is:pr is:open",
+    //     TYPE: "ISSUE",
+    //     link: `${BASE_URL}/mdn-content.json`
+    // },
+    // // Repository
+    // {
+    //     title: "LightWeight JavaScript repositories",
+    //     query: "lightweight language:javascript language:typescript sort:updated-desc",
+    //     TYPE: "REPOSITORY",
+    //     link: `${BASE_URL}/lightweight-javascript-repo.json`
+    // }
 ];
